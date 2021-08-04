17f6fd6319982d4159e4aed6a5dc8c0ddeed2d1b
liurenpeng <liurenpeng@kuaishou.com>
2021-08-04 23:01:49 +0800

    resample

--- Data/Stock.py
+++ Data/Stock.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
+"""
 @author: liurenpeng
 @time: 2021/8/3
-'''
+"""
 
 # https://www.joinquant.com/help/api/help#name:JQData
 from jqdatasdk import *
@@ -22,3 +22,12 @@ for stock_code in stocks:
     df = get_price(security=stock_code, end_date='2021-08-02',count=10, frequency='1d', panel=False)
     print(df)
     time.sleep(3)
+
+# resample函数的使用
+
+# 转换周期：日K转换为周K
+# 获取日K
+# 获取周K（当周的）：开盘价（当周第一天）、收盘价（当周最后一天）、最高价（当周）、最低价（当周）
+
+# 汇总统计：统计月成交量、成交额（sum）
+
